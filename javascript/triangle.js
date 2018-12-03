878dadfee78b8d3d71f0b138e303d01d1e7e3c68
John Omwanda <onkel@flemankenya@gmail.com>
2018-12-03 09:33:26 +0300

    edit readme file

--- javascript/triangle.js
+++ javascript/triangle.js
@@ -1,17 +1,18 @@
 var triangle = function() {
 
-  var a = document.getElementById('a').value;
-  var b = document.getElementById('b').value;
-  var c = document.getElementById('c').value;
+  var A = document.getElementById('a').value;
+  var B = document.getElementById('b').value;
+  var C = document.getElementById('c').value;
 
-  if ((A+B>C) && (B+C>A) && (A+C>B)) {
-    if (A==B && A==C && B==C) {
-        alert ("Equilateral Triangle");
-      } else if (A==B || A==C || B == C) {
-      alert ("Isosceles Triangle");
-    } else{
-      alert ("Scalene Triangle");
+  if ((A + B > C) && (B + C > A) && (A + C > B)) {
+    if (A == B && A == C && B == C) {
+      alert("Equilateral Triangle");
+    } else if (A == B || A == C || B == C) {
+      alert("Isosceles Triangle");
+    } else {
+      alert("Scalene Triangle");
     }
+  } else {
+    alert("Not a triangle")
+  }
 }
-  else {
-    alert ("Not a triangle")
---
+++ readme.md
@@ -0,0 +1,32 @@
+
+# Triangle Tracker
+<image = url("..images/triangles_750.gif");
+Triangle tracker is an online querry form that tells if dimensions constitute an Ascosceles, Equilateral
+or Scarlene Triangle or if the dimensions doesn't make up a triangle at all.
+
+# Author
+
+John Odhiambo Omwanda
+
+# Features!
+
+ Page constitutes a Javascript query form.
+
+ # How to use it:
+ - Open the site.
+ - Input triangle dimensions into A B and C side fields.
+ - Letters A B and C represents the three sides of a triangle.
+ - Click SUBMIT
+ - Once submitted an alert with the Triangle type appears .
+
+ # link
+
+ https://omwanda.github.io/Triangle-type/
+
+# License
+----
+
+MIT
+
+
+**Free to use**
